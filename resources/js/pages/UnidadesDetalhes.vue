a9e156752e479c0f278a555b666e07702cb915c6
Mauricio Amorim <mjvamorim@gmail.com>
2022-11-25 16:05:31 +0000

    Passando parametros Vue Router

--- resources/js/pages/UnidadesDetalhes.vue
+++ resources/js/pages/UnidadesDetalhes.vue
@@ -1,11 +1,11 @@
 <template>
   <v-card elevation="2" outlined shaped>
-    <v-card-title> Unidade: {{ unidade_id }} </v-card-title>
+    <v-card-title> Unidade: {{ unidade.descricao }}</v-card-title>
     <v-card-text> </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn color="blue darken-1" text to="/admin/">Voltar</v-btn>
-      <v-btn color="primary" text @click="">Executar</v-btn>
+      <!-- <v-btn color="primary" text @click="">Executar</v-btn> -->
     </v-card-actions>
   </v-card>
 </template>
@@ -13,13 +13,21 @@
 <script>
 export default {
   data: () => ({
-    unidade_id: 0
+    unidade_id: 0,
+    unidade: {},
   }),
   created() {
-    this.unidade_id = this.$router.query.id;
+    this.unidade_id = this.$route.query.id;
     console.log(this.unidade_id);
+    axios
+      .get("/api/unidades/" + this.unidade_id)
+      .then((response) => {
+        this.unidade = response.data;
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
   },
-  methods: {}
+  methods: {},
 };
 </script>
 <style scooped></style>
