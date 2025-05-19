36216f5100a4d1aad0ba7aaa74136c0b51ebb056
mjvamorim <mjvamorim@gmail.com>
2025-05-19 15:33:51 -0300

    Comment out queue worker command in condo.bat for clarity

--- condo.bat
+++ condo.bat
@@ -1,5 +1,6 @@
 cd c:\workspace\pegasus\condo
 php artisan config:cache
+@REM php artisan queue:work
 start /b php artisan serve 
 @REM start /b php artisan queue:work --queue=default --tries=3 --timeout=60
 start http://localhost:8000
